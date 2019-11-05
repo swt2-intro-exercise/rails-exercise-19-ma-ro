f645ae464056163335baea349e4441e412b78b71
Marc <marckyu5@gmail.com>
2019-11-05 17:34:58 +0100

    add name test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,5 +5,11 @@ describe "Author create", :type => :model do
     expect(author.first_name).to eq("Alan")
     expect(author.last_name).to eq("Turing")
     expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
-    end
+  end
+
+  it 'should ' do
+    author =  Author.new({first_name: "Alan", last_name: "Turing" , homepage: "http://wikipedia.org/Alan_Turing"})
+
+    expect(author.name).to eq("Alan Turing")
+  end
 end
