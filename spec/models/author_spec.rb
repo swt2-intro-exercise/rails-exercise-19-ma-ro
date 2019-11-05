a22ddbcd3a98a5a4bdab56bacff11da0fe262b1b
Marc <marckyu5@gmail.com>
2019-11-05 18:32:40 +0100

    refactor test for validation last_name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,7 +1,6 @@
 describe "Author create", :type => :model do
   it 'should ' do
     author =  Author.new({first_name: "Alan", last_name: "Turing" , homepage: "http://wikipedia.org/Alan_Turing"})
-
     expect(author.first_name).to eq("Alan")
     expect(author.last_name).to eq("Turing")
     expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
@@ -12,8 +11,8 @@ describe "Author create", :type => :model do
     expect(author.name).to eq("Alan Turing")
   end
 
-  it "should" do
-    author =  Author.new({first_name: "Alan", last_name: "" , homepage: "http://wikipedia.org/Alan_Turing"})
+  it "if no lastname then redirect" do
+    author =  Author.new({first_name: "Alan", last_name: nil, homepage: "http://wikipedia.org/Alan_Turing"})
     expect(author).to_not be_valid
   end
 end
