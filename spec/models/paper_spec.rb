33a13f039d118fedde46069559f6559fd7e4ce79
Marc <marckyu5@gmail.com>
2019-11-06 14:57:58 +0100

    add validation for papers

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should ' do
+    paper =  Paper.new({title: "Alan", venue: "Turing" , year: 1})
+    expect(paper.title).to eq("Alan")
+    expect(paper.venue).to eq("Turing")
+    expect(paper.year).to eq(1)
+  end
+
+  it "if no venue then redirect" do
+    paper =  Paper.new({title: "Alan", venue: nil, year: 1})
+    expect(paper).to_not be_valid
+  end
+
+  it "should redirect if year is no number" do
+    paper =  Paper.new({title: "Alan", venue: "Turin", year: "hi"})
+    expect(paper).to_not be_valid
+  end
 end
